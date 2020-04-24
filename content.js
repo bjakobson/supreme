c3bf6163a07bb5eb5a21e2da4efcf85c60348323
bjakobson <brandonjakobson@gmail.com>
2020-04-24 07:45:42 -0700

    new bot2
    Signed-off-by: bjakobson <brandonjakobson@gmail.com>

--- content.js
+++ content.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 $( document ).ready(function() {
 
@@ -87,24 +86,3 @@ $( document ).ready(function() {
 
 
 //          //"matches": ["https://www.supremenewyork.com/checkout", "https://www.supremenewyork.com/shop/jackets/pk2tbsagx/z60yvnd1f", "https://www.supremenewyork.com/shop/jackets/yqaoeynuh", "https://www.supremenewyork.com/shop/jackets/na6obt059", "https://www.supremenewyork.com/shop/jackets/rtzg8fjol", "https://www.supremenewyork.com/shop/jackets/l7zb3rwls", "https://www.supremenewyork.com/shop/shirts/nrla6isyw/rtkhre1sx"],
-=======
-document.getElementById("order_billing_name").value = "Brandon Jakobson"
-document.getElementById("order_email").value = "brandonjakobson@GMAIL.COM"
-document.getElementById("order_tel").value = "4256060298"
-document.getElementById("bo").value = "3403 219th PL SE"
-document.getElementById("order_billing_zip").value = "98075"
-document.getElementById("order_billing_city").value = "Sammamish"
-document.getElementById('order_billing_state').selectedIndex=55;//replace 20 with state number (0=1)
-document.getElementById("rnsnckrn").value = "4701 3203 2281 5966"
-//ocument.getElementById("orcer").value = "REPLACE THIS CREDIT CARD CVV"
-document.getElementById('credit_card_month').selectedIndex=4;
-document.getElementById('credit_card_year').selectedIndex=5;//replace 3 with card value (3=2022)
-//$('#order_terms').attr('checked',true)
-
-
-
-
-
-
-
->>>>>>> 5bfd66cc6bd9b0b750e80c3820ba6c27e1065b64
